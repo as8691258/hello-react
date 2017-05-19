149c38e9019f6fd61c887e1ca7bef0ef1aaef6bf
liujian <liu-885@163.com>
2017-05-19 20:10:43 +0800

    redux + react-redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
-import './index.css';
-import CommentApp from "./comment2/CommentApp";
+// import React, { Component } from 'react'
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import CommentApp from "./comment2/CommentApp";
+//
+//
+// ReactDOM.render(
+//     <CommentApp />,
+//     document.getElementById('root')
+// )
 
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import CommentApp from './containers/CommentApp'
+import commentsReducer from './reducers/comments'
+import './index.css'
+
+const store = createStore(commentsReducer)
 
 ReactDOM.render(
-    <CommentApp />,
+    <Provider store={store}>
+        <CommentApp />
+    </Provider>,
     document.getElementById('root')
-)
+);
